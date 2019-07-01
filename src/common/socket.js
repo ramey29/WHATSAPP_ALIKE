069eae9fc01bc495ccac123f9fcd23b61ee28c04
RB <rb@BBMDVRAMESHWAR.local>
2019-07-01 14:19:02 +0530

    dummy commit

--- src/common/socket.js
+++ src/common/socket.js
@@ -1,8 +1,3 @@
-let socket = io();
-socket = io.connect('http://localhost:3000');
-socket.on('connect', function(data) {
-    socket.emit('join', 'Hello World from client');
- });
 export const emit = (eventname, data) => {
     socket.emit(eventname, data);
 };
